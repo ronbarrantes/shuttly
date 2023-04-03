7382d393d163a8419ce3b3128e6a5b284724d45b
ronbarrantes <ronbarrantes@gmail.com>
2023-04-03 15:49:26 -0700

    Added some func to the button

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,15 @@
-import { Button } from "ui";
+import { useState } from 'react'
+import { Button } from 'ui'
 
 export default function Web() {
+  const [state, setState] = useState<string>()
+
+  const text = state ?? 'WEB'
+
   return (
     <div>
-      <h1>Web</h1>
-      <Button />
+      <h1>{text}</h1>
+      <Button onClick={() => setState('HELLO RON')} />
     </div>
-  );
+  )
 }
