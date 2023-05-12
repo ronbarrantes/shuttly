6f429b10fa6ba726fe91bc5cbd73d9f5b029cad5
Ron Barrantes <ronbarrantes@gmail.com>
2023-05-12 16:46:45 -0700

    Add add prisma (#29)

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,48 +1,75 @@
-import { Metadata } from "next";
-import { Button, Card } from "ui";
+import { Metadata } from 'next'
+import { Button, Card } from 'ui'
+import { prisma } from 'db'
 
 const CARD_CONTENT = [
   {
-    title: "Caching Tasks",
-    href: "https://turbo.build/repo/docs/core-concepts/caching",
-    cta: "Read More",
+    title: 'Caching Tasks',
+    href: 'https://turbo.build/repo/docs/core-concepts/caching',
+    cta: 'Read More',
   },
   {
-    title: "Running Tasks",
-    href: "https://turbo.build/repo/docs/core-concepts/monorepos/running-tasks",
-    cta: "Read More",
+    title: 'Running Tasks',
+    href: 'https://turbo.build/repo/docs/core-concepts/monorepos/running-tasks',
+    cta: 'Read More',
   },
   {
-    title: "Configuration Options",
-    href: "https://turbo.build/repo/docs/reference/configuration",
-    cta: "Read More",
+    title: 'Configuration Options',
+    href: 'https://turbo.build/repo/docs/reference/configuration',
+    cta: 'Read More',
   },
-];
+]
 
 export const metadata: Metadata = {
-  title: "Web - Turborepo Example",
-};
+  title: 'Web - Turborepo Example',
+}
 
-export default function Home() {
+const getAllPeople = async () => {
+  const passengers = await prisma.passenger.findMany()
+  return passengers
+}
+
+const getCompanies = async () => {
+  const passengers = await prisma.company.findMany()
+  return passengers
+}
+
+export default async function Home() {
+  const passengers = await getAllPeople()
+
+  // console.log('companies', companies)
+
+  console.log('PASSENGERS --->>', passengers)
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
-        <h1 className="mx-auto text-center text-6xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <main className="w-auto px-4 pt-16 pb-8 mx-auto sm:pt-24 lg:px-8">
+        <h1 className="mx-auto text-6xl font-extrabold tracking-tight text-center text-white sm:text-7xl lg:text-8xl xl:text-8xl">
           Web
-          <span className="block bg-gradient-to-r from-brandred to-brandblue bg-clip-text text-transparent px-2">
+          <span className="block px-2 text-transparent bg-gradient-to-r from-brandred to-brandblue bg-clip-text">
             Turborepo Example
           </span>
         </h1>
-        <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
+        <div className="max-w-xl mx-auto mt-5 sm:flex sm:justify-center md:mt-8">
           <Button />
         </div>
 
-        <div className="mt-12 grid grid-cols-1 sm:grid-cols-3 gap-4 place-content-evenly">
+        <div className="grid grid-cols-1 gap-4 mt-12 sm:grid-cols-3 place-content-evenly">
           {CARD_CONTENT.map((card) => (
             <Card key={card.title} {...card} />
           ))}
         </div>
+
+        <div className="flex flex-wrap max-w-3xl gap-2 m-auto text-white">
+          {passengers.map((passenger) => {
+            return (
+              <div key={passenger.id}>
+                <h1>{passenger.name}</h1>
+              </div>
+            )
+          })}
+        </div>
       </main>
     </div>
-  );
+  )
 }
