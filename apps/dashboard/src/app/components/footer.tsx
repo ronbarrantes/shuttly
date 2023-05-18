d0517268217ac5a0a05e70b8a57e9ea4238aae83
ronbarrantes <ronbarrantes@gmail.com>
2023-05-18 12:06:49 -0700

    Change footer

--- apps/dashboard/src/app/components/footer.tsx
+++ apps/dashboard/src/app/components/footer.tsx
@@ -1,10 +1,7 @@
 export const Footer = () => {
   return (
     <footer className="justify-self-end">
-      <div>
-        No clue what the footer will contain, maybe contact for when the app
-        breaks
-      </div>
+      <div>Shuttly app</div>
     </footer>
-  );
-};
+  )
+}
