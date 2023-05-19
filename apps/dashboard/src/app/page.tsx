535bca168fb5602cbf28c3d8f9199deb1a7e3e80
ronbarrantes <ronbarrantes@gmail.com>
2023-05-19 16:03:16 -0700

    Remove the turbo example h1

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -26,12 +26,6 @@ export default async function Home() {
 
   return (
     <PageLayout title="Dashboard">
-      <h1 className="mx-auto text-center text-6xl font-extrabold tracking-tight sm:text-7xl lg:text-8xl xl:text-8xl">
-        Web
-        <span className="from-brandred to-brandblue block bg-gradient-to-r bg-clip-text px-2 text-transparent">
-          Turborepo Example
-        </span>
-      </h1>
       <DashboardTable rides={rides} />
     </PageLayout>
   )
