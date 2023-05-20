7daf4fdf6a2d31607ce8425f5a2e60ba3dc2e912
ronbarrantes <ronbarrantes@gmail.com>
2023-05-19 17:07:08 -0700

    Add invitation page

--- apps/dashboard/src/app/invitation/page.tsx
+++ apps/dashboard/src/app/invitation/page.tsx
@@ -1,3 +1,14 @@
+import { prisma } from 'db'
+import { useUser } from '@clerk/nextjs'
+
+const getInvitation = async () => {
+  const invitation = await prisma.invitation.findUnique({
+    where: {},
+  })
+}
+
+const acceptInvitation = async () => {}
+
 export default function Invitation() {
-  return <h1>Invitation</h1>
+  return <div></div>
 }
