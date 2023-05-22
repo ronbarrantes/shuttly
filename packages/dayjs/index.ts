1f8c58f850c62a56989e4d4be36ff6a54ad6baab
ronbarrantes <ronbarrantes@gmail.com>
2023-05-22 15:40:31 -0700

    tweak some imports

--- packages/dayjs/index.ts
+++ packages/dayjs/index.ts
@@ -1,3 +1,14 @@
+import dayjs from 'dayjs';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import isBetween from 'dayjs/plugin/isBetween';
+import isToday from 'dayjs/plugin/isToday';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import timeZone from 'dayjs/plugin/timezone';
+import toArray from 'dayjs/plugin/toArray';
+import utc from 'dayjs/plugin/utc';
+
 import 'dayjs/locale/ar';
 import 'dayjs/locale/bg';
 import 'dayjs/locale/cs';
@@ -23,16 +34,6 @@ import 'dayjs/locale/uk';
 import 'dayjs/locale/vi';
 import 'dayjs/locale/zh-cn';
 import 'dayjs/locale/zh-tw';
-import dayjs from 'dayjs';
-import advancedFormat from 'dayjs/plugin/advancedFormat';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
-import isBetween from 'dayjs/plugin/isBetween';
-import isToday from 'dayjs/plugin/isToday';
-import localizedFormat from 'dayjs/plugin/localizedFormat';
-import relativeTime from 'dayjs/plugin/relativeTime';
-import timeZone from 'dayjs/plugin/timezone';
-import toArray from 'dayjs/plugin/toArray';
-import utc from 'dayjs/plugin/utc';
 
 dayjs.extend(customParseFormat);
 dayjs.extend(isBetween);
