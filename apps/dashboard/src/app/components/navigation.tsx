857960dda3347c74e62d181554eef78bff22831a
ronbarrantes <ronbarrantes@gmail.com>
2023-05-22 14:01:38 -0700

    Change stuff in nav

--- apps/dashboard/src/app/components/navigation.tsx
+++ apps/dashboard/src/app/components/navigation.tsx
@@ -2,16 +2,50 @@ import Link from 'next/link'
 import React from 'react'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 
+type LinkItemsProps = {
+  pathname?: string
+  links: {
+    name: string
+    href: string
+  }[]
+}
+
+const mainNavLinks: LinkItemsProps = {
+  pathname: '/',
+  links: [
+    {
+      name: 'Dashboard',
+      href: '/',
+    },
+    {
+      name: 'Settings',
+      href: '/settings',
+    },
+  ],
+}
+
+const settingsPageLinks: LinkItemsProps = {
+  pathname: '/settings',
+  links: [],
+}
+
+const LinkItems = ({ links, pathname = '/' }: LinkItemsProps) => {
+  return (
+    <>
+      {links.map((link) => (
+        <li key={link.name}>
+          <Link href={`${pathname}${link.href}`}>{link.name}</Link>
+        </li>
+      ))}
+    </>
+  )
+}
+
 export const MainNavigation = () => {
   return (
     <nav>
       <ul className="flex items-center gap-3 border border-red-500">
-        <li>
-          <Link href={'/'}>Dashboard</Link>
-        </li>
-        <li>
-          <Link href={'/settings'}>Settings</Link>
-        </li>
+        <LinkItems {...mainNavLinks} />
         <li>
           <SigninNavigation />
         </li>
