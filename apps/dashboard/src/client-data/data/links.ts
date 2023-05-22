5063d19154024988e8f634f11e63dd31ac827712
ronbarrantes <ronbarrantes@gmail.com>
2023-05-22 14:29:48 -0700

    Add links data

---
+++ apps/dashboard/src/client-data/data/links.ts
@@ -0,0 +1,43 @@
+export type LinkItemsProps = {
+  pathname?: string
+  links: {
+    name: string
+    href: string
+  }[]
+}
+
+export const mainNavLinks: LinkItemsProps = {
+  pathname: '/',
+  links: [
+    {
+      name: 'Dashboard',
+      href: '/',
+    },
+    {
+      name: 'Settings',
+      href: '/settings',
+    },
+  ],
+}
+
+export const settingsPageLinks: LinkItemsProps = {
+  pathname: '/settings',
+  links: [
+    /**
+        <li>
+          <Link href={`${uri}/`}>Personal</Link>
+        </li>
+        <li>
+          <Link href={`${uri}/`}>Company</Link>
+        </li>
+        <li>
+          <Link href={`${uri}/`}>Drivers</Link>
+        </li>
+        <li>
+          <Link href={`${uri}/`}>Drivers</Link>
+        </li> 
+ 
+
+ */
+  ],
+}
