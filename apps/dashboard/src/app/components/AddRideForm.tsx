efd9530066372906207dd50b10b210eee96abf00
ronbarrantes <ronbarrantes@gmail.com>
2023-05-22 15:39:24 -0700

    Add the dialog to the form

--- apps/dashboard/src/app/components/AddRideForm.tsx
+++ apps/dashboard/src/app/components/AddRideForm.tsx
@@ -1,4 +1,6 @@
+'use client'
 import type { AddPassenger, AddRide } from '@actions/ride'
+import { Dialog } from '@components/Dialog'
 
 type AddRideFormProps = {
   addPassenger: AddPassenger
@@ -6,9 +8,22 @@ type AddRideFormProps = {
 }
 
 export const AddRideForm = ({ addPassenger, addRide }: AddRideFormProps) => {
+  // have a button to that will bring out a form in a modal
+
+  // the form for now will have just the fields to edit
+  // but eventually it will have a search feature that will look for
+  // passengers that have already been added to the database
+  // if they're not in the database it will just add them without much fuzz
+
   return (
     <div>
       <p>Add Ride Form</p>
+      <Dialog>
+        <Dialog.Trigger>Open Dialog</Dialog.Trigger>
+        <Dialog.Content title="Add a ride">
+          <p>Hello rider</p>
+        </Dialog.Content>
+      </Dialog>
     </div>
   )
 }
