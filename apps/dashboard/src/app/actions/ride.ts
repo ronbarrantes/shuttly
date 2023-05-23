dbab6691370aa84c7f9053cc12301faa3718cfef
ronbarrantes <ronbarrantes@gmail.com>
2023-05-23 13:54:09 -0700

    Start ride type

--- apps/dashboard/src/app/actions/ride.ts
+++ apps/dashboard/src/app/actions/ride.ts
@@ -1,10 +1,52 @@
 'use server'
 
 import { prisma } from 'db'
+import { z } from 'zod'
 
-export const addPassenger = () => {}
+export const addPassenger = async ({}) => {
+  console.log('ADDING A PASSENGER')
+}
 
-export const addRide = () => {}
+const RideObj = z.object({
+  name: z.string(),
+  address: z.string(),
+  phone: z.string(),
+  companyId: z.string(),
+  passengerId: z.string().optional(),
+  rides: z.array(
+    z.object({
+      rideType: z.enum(['pickup', 'dropoff']),
+      scheduledTime: z.date(),
+      altAddress: z.string().optional(),
+      driverId: z.string().optional(),
+      companyId: z.string(),
+    })
+  ),
+})
+
+export const addRide = async ({
+  name,
+  address,
+  phone,
+  companyId,
+  rides,
+  passengerId,
+}: RideType) => {
+  if (!passengerId) {
+  }
+
+  const passenger = await prisma.passenger.create({
+    data: {
+      name,
+      address,
+      phone,
+      companyId,
+      rides: {
+        create: rides,
+      },
+    },
+  })
+}
 
 export const getAllRides = async () => {
   const rides = await prisma.ride.findMany({
@@ -23,3 +65,4 @@ export const getAllRides = async () => {
 export type AddPassenger = typeof addPassenger
 export type AddRide = typeof addRide
 export type InferredRide = Awaited<ReturnType<typeof getAllRides>>[0]
+export type RideType = z.infer<typeof RideObj>
