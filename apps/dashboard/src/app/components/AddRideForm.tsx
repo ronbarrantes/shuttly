d9c8c29fd3ad2d3820616bf458561edd13ae9d00
ronbarrantes <ronbarrantes@gmail.com>
2023-05-23 12:46:18 -0700

    Add some inputs

--- apps/dashboard/src/app/components/AddRideForm.tsx
+++ apps/dashboard/src/app/components/AddRideForm.tsx
@@ -1,6 +1,12 @@
 'use client'
 import type { AddPassenger, AddRide } from '@actions/ride'
 import { Dialog } from '@components/Dialog'
+import {
+  Controller,
+  FormProvider,
+  useForm,
+  useFormContext,
+} from 'react-hook-form'
 
 type AddRideFormProps = {
   addPassenger: AddPassenger
@@ -8,12 +14,20 @@ type AddRideFormProps = {
 }
 
 export const AddRideForm = ({ addPassenger, addRide }: AddRideFormProps) => {
-  // have a button to that will bring out a form in a modal
-
   // the form for now will have just the fields to edit
   // but eventually it will have a search feature that will look for
   // passengers that have already been added to the database
   // if they're not in the database it will just add them without much fuzz
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  // = useForm({
+  //   defaultValues: {},
+  // })
 
   return (
     <div>
@@ -21,7 +35,27 @@ export const AddRideForm = ({ addPassenger, addRide }: AddRideFormProps) => {
       <Dialog>
         <Dialog.Trigger>Open Dialog</Dialog.Trigger>
         <Dialog.Content title="Add a ride">
-          <p>Hello rider</p>
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit((data) => console.log(data))}
+          >
+            <input
+              {...register('firstName')}
+              placeholder="First Name"
+              className="rounded-lg border border-black"
+            />
+            <input
+              {...register('lastName', { required: true })}
+              className="rounded-lg border border-black"
+            />
+            {errors.lastName && <p>Last name is required.</p>}
+            <input
+              className="rounded-lg border border-black"
+              {...register('age', { pattern: /\d+/ })}
+            />
+            {errors.age && <p>Please enter number for age.</p>}
+            <input type="submit" />
+          </form>
         </Dialog.Content>
       </Dialog>
     </div>
--- apps/dashboard/src/app/components/Dialog.tsx
+++ apps/dashboard/src/app/components/Dialog.tsx
@@ -71,7 +71,7 @@ const DialogContent = ({
   return (
     <DialogPrimitive.Portal>
       <DialogPrimitive.Overlay className="bg-blackA8 data-[state=open]:animate-overlayShow fixed inset-0" />
-      <DialogPrimitive.Content className="data-[state=open]:animate-contentShow fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+      <DialogPrimitive.Content className="data-[state=open]:animate-contentShow fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] text-black shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
         <DialogTitle>{title}</DialogTitle>
         {description && <DialogDescription>{description}</DialogDescription>}
         {children}
