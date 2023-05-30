3310339307d43651f0a36df29b8287c625e3a114
ronbarrantes <ronbarrantes@gmail.com>
2023-05-30 14:11:11 -0700

    Add a little comment

--- apps/dashboard/src/app/sign-in/page.tsx
+++ apps/dashboard/src/app/sign-in/page.tsx
@@ -1,6 +1,9 @@
 import { SignInButton } from '@clerk/nextjs'
 import { PageLayout } from '@components/page-layout'
 
+// TODO: come up with a solution so that the app can be used without being part of a company
+// maybe create a temporary fake company in order to be part of
+
 const SignInPage = () => {
   return (
     <PageLayout title="" noMenu>
