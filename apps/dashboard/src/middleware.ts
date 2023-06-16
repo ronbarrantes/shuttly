49b89ae9eb2512eb1a30e28b226558027a8c09e9
ronbarrantes <ronbarrantes@gmail.com>
2023-06-16 11:18:08 -0700

    Tryng to fix middleware

--- apps/dashboard/src/middleware.ts
+++ apps/dashboard/src/middleware.ts
@@ -5,18 +5,29 @@ export default authMiddleware({
   publicRoutes: ['/sign-in'],
   afterAuth: async (auth, req, _evt) => {
     // handle users who aren't authenticated
+
+    console.log('### Not signed in ###')
+
     if (!auth.userId && !auth.isPublicRoute) {
       const signInUrl = new URL('/sign-in', req.url)
       signInUrl.searchParams.set('redirect_url', req.url)
+
+      console.log('### Redirect to sign in ###')
       return NextResponse.redirect(signInUrl)
     }
 
     // once authenticated, checks if user has an account
     // it does this by checking if the user's metadata has been added to clerk
     // this happens during the invitation process
+
+    console.log('### User check ###')
+
     let usr = null
     if (auth.userId) usr = await clerkClient.users.getUser(auth.userId)
 
+    console.log('USR ====>>', usr?.id)
+
+    console.log('### Signed in Check, but not assigned ###')
     if (
       auth.userId &&
       !usr?.privateMetadata.companyId &&
@@ -26,13 +37,9 @@ export default authMiddleware({
       return NextResponse.redirect(invitation)
     }
 
-    if (
-      usr?.privateMetadata.companyId &&
-      req.nextUrl.pathname === '/invitation'
-    ) {
-      const dashboard = new URL('/', req.url)
-      return NextResponse.redirect(dashboard)
-    }
+    console.log('### Redirect to dashboard ###')
+
+    // console.log('### Redirect to dashboard ###')
   },
 })
 
