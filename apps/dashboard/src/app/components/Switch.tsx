3f6056e14912c664c04ad849222dd6a9136acb7a
ronbarrantes <ronbarrantes@gmail.com>
2023-06-17 18:32:17 -0700

    Update switch

--- apps/dashboard/src/app/components/Switch.tsx
+++ apps/dashboard/src/app/components/Switch.tsx
@@ -4,30 +4,36 @@ import * as SwitchPrimitive from '@radix-ui/react-switch'
 import React from 'react'
 
 interface SwitchProps {
-  label: string
-  // id: string
+  label?: string
   checked?: boolean
   name: string
   onCheckedChange: (checked: boolean) => void
-  // value?: 'yes' | 'no'
+  useLabel?: boolean
 }
 
 export const Switch = React.forwardRef(
   (
-    { label, name, checked, onCheckedChange, ...rest }: SwitchProps,
+    {
+      label,
+      name,
+      checked,
+      useLabel = false,
+      onCheckedChange,
+      ...rest
+    }: SwitchProps,
     ref: React.Ref<HTMLButtonElement>
   ) => {
     return (
-      <div
-        className="flex items-center"
-        // style={{ display: 'flex', alignItems: 'center' }}
-      >
-        <label
-          className="pr-[15px] text-[15px] leading-none text-black"
-          htmlFor={name}
-        >
-          {label}
-        </label>
+      <div className="flex items-center">
+        {useLabel && (
+          <label
+            className="pr-[15px] text-[15px] leading-none text-black"
+            htmlFor={name}
+          >
+            {label}
+          </label>
+        )}
+
         <SwitchPrimitive.Root
           className="relative h-[25px] w-[42px] cursor-default rounded-full border border-slate-400 bg-slate-900 outline-none focus:shadow-[0_0_0_2px]  data-[state=checked]:bg-black"
           name={name}
@@ -38,8 +44,6 @@ export const Switch = React.forwardRef(
         >
           <SwitchPrimitive.Thumb className="block h-[21px] w-[21px] translate-x-0.5 rounded-full bg-indigo-600 shadow-[0_1px_1px]  transition-transform duration-100 will-change-transform data-[state=checked]:translate-x-[19px]" />
         </SwitchPrimitive.Root>
-
-        {/* <p>Hello world</p> */}
       </div>
     )
   }
